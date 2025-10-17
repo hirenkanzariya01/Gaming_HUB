dc9d49a149428b5448f484bd76bb4c5d9d4e2e47
hiren.kanzariya01 <kanzariyahiren655@gmail.com>
2025-10-17 14:09:12 +0530

    create a route and multipal pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import './App.css'
+import Navbar from './componants/Navbar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './Pages/HomePage'
+import Shop from './Pages/Shop'
+import Product from './Pages/Product'
+import Contact from './Pages/Contact'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <BrowserRouter>
+        <div className='MainApp'>
+          <Navbar />
+
+          <Routes>
+            <Route path='/' element={<HomePage />}></Route>
+            <Route path='/Shop' element={<Shop />}></Route>
+            <Route path='/Product' element={<Product />}></Route>
+            <Route path='/Contact' element={<Contact />}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   )
 }
 
